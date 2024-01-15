334fd26d680a524354087ea14d738b72d261c21e
Elisa Rivero Perez <erivero-@c3r8s5.42malaga.com>
2024-01-15 15:19:55 +0100

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 13:16:13 by erivero-          #+#    #+#             */
-/*   Updated: 2024/01/15 13:55:40 by erivero-         ###   ########.fr       */
+/*   Updated: 2024/01/15 15:16:52 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int	main(int ac, char **av)
 				std::cout << (char)toupper(av[i][j]);
 		}
 		std::cout << std::endl;
-
 	}
 	return (0);
 }
