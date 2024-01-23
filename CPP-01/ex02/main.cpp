7d4e7e34b19d76f110043e42c3bb8e6194521395
Elisa Rivero Perez <erivero-@c3r9s2.42malaga.com>
2024-01-23 15:53:50 +0100

    CPP01/ex02

--- CPP-01/ex02/main.cpp
+++ CPP-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:38:36 by erivero-          #+#    #+#             */
-/*   Updated: 2024/01/23 15:50:40 by erivero-         ###   ########.fr       */
+/*   Updated: 2024/01/23 15:53:27 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int	main(void)
 	std::string str	= "HI THIS IS BRAIN";
 	std::string *ptr = &str;
 	std::string	&ref = str;
-
+// imprimir las direcciones de memoria
 	std::cout << &str << std::endl;
 	std::cout << ptr << std::endl;
 	std::cout << &ref << std::endl;
+// imprimir el valor de la string	
 	std::cout << str << std::endl;
 	std::cout << *ptr << std::endl;
 	std::cout << ref << std::endl;
