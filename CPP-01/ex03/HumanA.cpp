8530a85be8c069542a382fac1ae33c31315fdc8c
erivero-p <elrivper@gmail.com>
2024-01-30 12:13:33 +0100

    01/ex03

--- CPP-01/ex03/HumanA.cpp
+++ CPP-01/ex03/HumanA.cpp
@@ -6,20 +6,14 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 11:51:38 by erivero-          #+#    #+#             */
-/*   Updated: 2024/01/25 11:05:36 by erivero-         ###   ########.fr       */
+/*   Updated: 2024/01/30 12:13:14 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-/* HumanA::HumanA(std::string nick, Weapon &weap)
-{
-	name = nick;
-	this->wp = weap;
-} */
 HumanA::HumanA(std::string nick, Weapon &wp) : name(nick), weapon(wp) {}
-//resulta que se puede dar valor a las cosas en la declaración del constructor
-// en lugar de en el cuerpo del mismo
+
 HumanA::~HumanA() {}
 
 void	HumanA::attack()
