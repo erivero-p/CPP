492ae30785072e444dd928c99332bd980ee439a7
Elisa Rivero Perez <erivero-@c2r2s1.42malaga.com>
2024-03-06 14:07:04 +0100

    ex01

--- CPP-04/ex02/Animal.cpp
+++ CPP-04/ex02/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:53:48 by erivero-          #+#    #+#             */
-/*   Updated: 2024/02/19 11:42:33 by erivero-         ###   ########.fr       */
+/*   Updated: 2024/03/06 14:05:17 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,6 @@ Animal&	Animal::operator=(const Animal &src) {
 Animal::~Animal(void) {
 
 }
-
-/* void	Animal::makeSound(void) const {
-
-	std::cout << "\033[1;33m(?)" << std::endl << "\033[0m";
-} */
 
 std::string	Animal::getType(void) const {
 	return(this->type);
