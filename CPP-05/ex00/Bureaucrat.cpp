04d997dc81021df86cd1c2eb07f3a5f76b180631
Elisa Rivero Perez <erivero-@c2r4s1.42malaga.com>
2024-03-19 12:47:25 +0100

    dislexic again

--- CPP-05/ex00/Bureaucrat.cpp
+++ CPP-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 14:46:34 by erivero-          #+#    #+#             */
-/*   Updated: 2024/03/19 11:40:49 by erivero-         ###   ########.fr       */
+/*   Updated: 2024/03/19 12:46:58 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ Bureaucrat::Bureaucrat(const std::string nm, int gr) : name(nm) {
 	try {
 		if (gr > 0 && gr < 151)
 			grade = gr;
-		else if (gr < 1)
+		else if (gr > 150)
 			throw(Bureaucrat::GradeTooLowException());
 		else
 			throw(Bureaucrat::GradeTooHighException());
@@ -43,8 +43,7 @@ Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src) {
 
 	if (this != &src)
 		this->grade = src.grade;
-	//el nombre no puede asiganrse porque es constante
-	//una vez declarado no se podrá cambiar
+	/* name cant be reasigned cause is const */
 	return (*this);
 }
 
