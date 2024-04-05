4eb7ceb107f149d6a0436b1f3c2b49203aefb5d4
Elisa Rivero Perez <erivero-@c3r9s1.42malaga.com>
2024-04-05 12:44:39 +0200

    mutant stack

--- CPP-08/ex02/MutantStack.hpp
+++ CPP-08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 12:38:46 by erivero-          #+#    #+#             */
-/*   Updated: 2024/04/05 12:38:59 by erivero-         ###   ########.fr       */
+/*   Updated: 2024/04/05 12:44:35 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,3 @@ public:
 };
 
 #include "MutantStack.tpp"
-
-/* #pragma once
-
-# include <iostream>
-# include <string>
-# include <exception>
-# include <stack>
-
-template <typename T>
-class MutantStack: public std::stack<T>
-{
-	public:
-		typedef typename std::stack<T>::container_type::iterator iterator;
-		MutantStack(void) {};
-		MutantStack(const MutantStack &src): std::stack<T>(src){};
-		MutantStack &operator=(const MutantStack &src){
-			std::stack<T>::operator=(src);
-			return (*this);
-		};
-		~MutantStack(){};
-		iterator begin() {
-			return (this->c.begin());
-		};
-		iterator end() {
-			return (this->c.end());
-		};
-}; */
